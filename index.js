c8fe8f7a2682cd893fb8891875942fb7f2666bbb
Joyce Njogu <joycenjogu127@gmail.com>
2025-09-08 19:54:26 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Function expression called divide
+const divide = function (a, b) {
+  return a / b;
+};
+
+// 2. Arrow function called square
+const square = (x) => x * x;
+
+// 3. Arrow function called add
+const add = (a, b) => a + b;
